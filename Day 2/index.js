64a345e5201eecbd1e6fce42a075042cac74b27f
mythless <adimk3107@gmail.com>
2026-01-22 22:38:33 +0530

    day 2

---
+++ Day 2/index.js
@@ -0,0 +1,21 @@
+
+function add(a,b){
+    return a + b
+}
+
+function sub(a,b){
+    return a-b
+}
+
+function mul(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    if(b ===0)return "cannot be zero"
+    return a/b
+}
+
+console.log(add(5,61))
+console.log(mul(55,65));
+ 
