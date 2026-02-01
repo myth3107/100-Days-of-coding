52bc916c9ed9739389d0fb3e008775d6612acf43
mythless <adimk3107@gmail.com>
2026-02-02 02:48:43 +0530

    Day-3

---
+++ Day 3/index.js
@@ -0,0 +1,17 @@
+let input = document.querySelector("input");
+let button  = document.querySelector("button");
+
+
+input.addEventListener("input",(task)=>{
+    if(task.data !== null){
+console.log(task.data);
+    }   
+})
+
+function addTodo(){
+    <div>  
+        <input type="checkbox"></input>
+        console.log(task.data);
+        
+    </div>
+}
