7b0409f856fc147ea68d1e4ff895fcb47cbe8afc
mythless <adimk3107@gmail.com>
2026-02-03 02:17:27 +0530

    div of todos with checkbox and delete. with JS

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,10 +1,31 @@
 let form = document.querySelector("form");
 let input = document.querySelectorAll("input")
+let main = document.querySelector(".main")
 
 form.addEventListener("submit", (dets)=>{
 dets.preventDefault();
-console.log(input[0].value);
+// console.log(input[0].value);
 
+let card = document.createElement("div");
+card.classList.add("card");
+
+let checkbox = document.createElement("input")
+ checkbox.type = "checkbox";
+ checkbox.classList.add("checkbox");
+
+ let h2 = document.createElement("h2");
+ h2.classList.add("h2");
+ h2.innerText = input[0].value;
+
+ let del = document.createElement("button")
+ del.classList.add("button");
+ del.innerText = "Delete";
+
+ card.appendChild(checkbox);
+ card.appendChild(h2);
+ card.appendChild(del);
+
+ main.appendChild(card);
 
 
 })
